7b8cc926c153b93266dc9330d8a7fc40916bbd09
Daniel Ahlberg <daniel.ahlberg@fev.se>
2022-12-14 09:41:20 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flow-nodejs
+
+Use .env file to configure access to Flow
+```
+FLOW=https://flow.kurbit.se
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+require('dotenv').config()
+const bent = require('bent');
+
+exports.httpCall = async function(authorization, endpoint, method, {objectId = null, data = {}, params = {}}) {
+    const headers = {
+      'Authorization': 'Basic ' + authorization,
+      'Flow-Request-Method': method      
+    }
+  
+    if (Object.keys(params).length !== 0) {
+        headers['Flow-Request-Parameter'] = JSON.stringify({"parameters": params})
+    }
+
+    if (objectId != null) {
+        headers['Flow-Request-Object'] = objectId
+    }
+
+    if (endpoint.split('/',1) == "catalogue") {
+        path = 'api/product' + endpoint + '/'
+    } else {
+        path = 'api/' + endpoint.split('/',1) + '/' + endpoint + '/'
+    }
+
+    const flow = bent(process.env.FLOW, 'POST', 'json')
+  
+    return await flow(path, data, headers)
+}
+
+exports.httpList = async function(authorization, endpoint, {objectId = null, searchItems = null, sortBy = null, sortOrder = "ASC"}) {
+    let params = { "condition": "" }
+
+    if (searchItems != null) {
+        params['condition'] = true
+        params['condition.values'] = []
+
+        for (const [key, value] of Object.entries(searchItems)) {
+            params['condition.values'].append({key: value})
+        }
+    }
+
+    if (sortBy != null) {
+        params['sorting'] = true
+        params['sorting.by'] = sortBy
+        params['sorting.order'] = sortOrder
+    }
+
+    return this.httpCall(authorization, endpoint, "LIST", {objectId: objectId, params: params})
+}
+
+
+exports.httpOpen = async function(authorization, endpoint, {objectId = null, data = {}}) {
+    return this.httpCall(authorization, endpoint, "OPEN", {objectId: objectId, data: data})
+}
+
+exports.httpCreate = async function(authorization, endpoint, {objectId = null, data = {}}) {
+    return this.httpCall(authorization, endpoint, "CREATE", {objectId: objectId, data: data})
+}
+
+exports.httpUpdate = async function(authorization, endpoint, {objectId = null, data = {}}) {
+    return this.httpCall(authorization, endpoint, "UPDATE", {objectId: objectId, data: data})
+}
+
+exports.httpDelete = async function(authorization, endpoint, {objectId = null, data = {}}) {
+    return this.httpCall(authorization, endpoint, "DELETE", {objectId: objectId, data: data})
+}
+
+
+exports.login = async function(username,password) {
+    const headers = {'Authorization': 'Basic ' + new Buffer.from(username + ':' + password).toString('base64')}
+    const body = {"username": username, "password": password, "clientAddress": '0.0.0.0'}
+    const flow = bent(process.env.FLOW, 'POST', 'json', 200)
+
+    const response = await flow('api/auth/auth/login/', body, headers)
+    
+    if (response.success === true) {
+        return true
+    } else {
+        return false
+    }
+}
